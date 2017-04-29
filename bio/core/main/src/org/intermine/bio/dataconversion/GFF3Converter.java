2fdb04c2f77e72d38cced9be82e5f6538757d5a8
Deepak Unni <deepak.unni3@gmail.com>
2017-04-28 21:24:54 -0500

    a way to handle source for sequence (needs discussion)

--- bio/core/main/src/org/intermine/bio/dataconversion/GFF3Converter.java
+++ bio/core/main/src/org/intermine/bio/dataconversion/GFF3Converter.java
@@ -293,7 +293,7 @@ public class GFF3Converter extends DataConverter
         }
         String refId = identifierMap.get(primaryIdentifier);
         handler.clear(); // get rid of previous record Items from handler
-        Item seq = getSeq(record.getSequenceID());
+        Item seq = getSeq(record.getSequenceID(), record.getSource());
         String className = TypeUtil.javaiseClassName(term);
         String fullClassName = tgtModel.getPackageName() + "." + className;
         ClassDescriptor cd = tgtModel.getClassDescriptorByName(fullClassName);
@@ -707,7 +707,7 @@ public class GFF3Converter extends DataConverter
      * @return return/create item of class seqClsName for given identifier
      * @throws ObjectStoreException if the Item can't be stored
      */
-    private Item getSeq(String id)
+    private Item getSeq(String id, String source)
         throws ObjectStoreException {
         // the seqHandler may have changed the id used, e.g. if using an IdResolver
         String identifier = sequenceHandler.getSeqIdentifier(id);
@@ -722,7 +722,7 @@ public class GFF3Converter extends DataConverter
 
         Item seq = seqs.get(identifier);
         if (seq == null) {
-            seq = sequenceHandler.makeSequenceItem(this, identifier);
+            seq = sequenceHandler.makeSequenceItem(this, identifier, source);
             // sequence handler may choose not to create sequence
             if (seq != null) {
                 seq.addReference(getOrgRef());
