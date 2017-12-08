4042e09acbda98de9d8cc7c218ac349558c19850
Deepak Unni <deepak.unni3@gmail.com>
2017-12-08 17:31:33 -0600

    Modify expression-metadata source

--- bio/sources/expression-metadata/main/src/org/intermine/bio/dataconversion/ExpressionMetadataConverter.java
+++ bio/sources/expression-metadata/main/src/org/intermine/bio/dataconversion/ExpressionMetadataConverter.java
@@ -1,7 +1,7 @@
 package org.intermine.bio.dataconversion;
 
 /*
- * Copyright (C) 2002-2015 FlyMine
+ * Copyright (C) 2002-2016 FlyMine
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
@@ -30,20 +30,20 @@ import org.intermine.xml.full.Item;
 import org.apache.log4j.Logger;
 import org.intermine.util.FormattedTextParser;
 
-
 /**
- * 
+ *
  * @author
  */
 public class ExpressionMetadataConverter extends BioFileConverter
 {
     //
-    protected static final Logger LOG = Logger.getLogger(ExpressionMetadataConverter.class);
-    private String orgRefId;
-    private HashMap<String,Item> publicationItems = new HashMap<String, Item>();
-   private Map<String, String> genes = new HashMap<String, String>();
-    private static final String DATASET_TITLE = "Apis mellifera RNASeq Expression dataset";
-    private static final String DATA_SOURCE_NAME = "Apis mellifera RNASeq Expression dataset from NCBI SRA";
+    private static final String PLANT_ONTOLOGY = "Plant Ontology";
+    private HashMap<String,Item> sampleMap = new HashMap<String, Item>();
+    private HashMap<String,Item> ontologyTermMap = new HashMap<String, Item>();
+    private HashMap<String,Item> delayedItems = new HashMap<String, Item>();
+    private static final String DATASET_TITLE = "Maize Expression Metadata";
+    private static final String DATA_SOURCE_NAME = "Maize Expression Metadata from SRA";
+
     /**
      * Constructor
      * @param writer the ItemWriter used to handle the resultant items
@@ -52,80 +52,180 @@ public class ExpressionMetadataConverter extends BioFileConverter
     public ExpressionMetadataConverter(ItemWriter writer, Model model) {
         super(writer, model, DATA_SOURCE_NAME, DATASET_TITLE);
     }
+
     /**
-     * 
      *
      * {@inheritDoc}
      */
     public void process(Reader reader) throws Exception {
-        // assumes that the metadata file has unique entries
         Iterator<String[]> lineIter = FormattedTextParser.parseTabDelimitedReader(reader);
-       while (lineIter.hasNext()) {
-            String[] line = lineIter.next(); 
-            String sampleName = line[0];
-            String sampleMean = line[1];
-            String bioSample = line[2];	
-	String run = line[3];	
-	String experiment = line[4];	
-	String SRAStudy	= line[5];
-	String bioProject = line[6];	
-	String libraryLayout = line[7];	
-	String sample = line[8];	
-	String bioSample_description = line[9];	
-	String organ_group = line[10];	
-	String tissue_description = line[11];	
-	String PO_term	= line[12];
-	String PO_name	= line[13];
-	String growth_stage = line[14];	
-	String replicate = line[15];
-            Item item = createItem("ExpressionMetadata");
-            if (!sampleName.isEmpty()) {
-                item.setAttribute("sampleName", sampleName);
+        while(lineIter.hasNext()) {
+            String[] line = lineIter.next();
+            String sampleReplicateName = line[0];
+            String sampleName = line[1];
+            String sraBioSampleId = line[2];
+            String sraRunId = line[3];
+            String[] sraRunIdArray = sraRunId.split(";");
+            sraRunId = String.join(", ", sraRunIdArray);
+            String sraExperimentId = line[4];
+            String[] sraExperimentIdArray = sraExperimentId.split(";");
+            sraExperimentId = String.join(", ", sraExperimentIdArray);
+            String sraStudyId = line[5];
+            String sraBioProjectId = line[6];
+            String libraryLayout = line[7];
+            String sraSampleId = line[8];
+            String bioSampleDescription = line[9];
+            String organGroup = line[10];
+            String tissueDescription = line[11];
+            String poTerm = line[12].replaceAll(" ","");
+            String poTermName = line[13];
+            String growthStage = line[14];
+            String replicateNumber = line[15];
+
+            if (sampleName.isEmpty()) {
+                System.out.println("sampleName cannot be empty");
+                System.exit(1);
             }
-            else {
-                System.out.println("sampleName cannot be empty as it serves as a primaryIdentifier");
+            if (sampleReplicateName.isEmpty()) {
+                System.out.println("sampleReplicateName cannot be empty");
                 System.exit(1);
             }
 
-          
+            // create or get Sample item
+            Item sampleItem;
+            if (sampleMap.containsKey(sampleName)) {
+                sampleItem = sampleMap.get(sampleName);
+            }
+            else {
+                sampleItem = createItem("Sample");
+                sampleItem.setAttribute("name", sampleName);
+                sampleItem.setAttribute("sampleName", sampleName);
+                if (!sraStudyId.isEmpty()) sampleItem.setAttribute("sraStudyId", sraStudyId);
+                if (!sraBioProjectId.isEmpty()) sampleItem.setAttribute("sraBioProjectId", sraBioProjectId);
+                if (!libraryLayout.isEmpty()) sampleItem.setAttribute("libraryLayout", libraryLayout);
+                if (!growthStage.isEmpty()) sampleItem.setAttribute("growthStage", growthStage);
+                Item poTermItem = getPlantOntologyTerm(poTerm, poTermName);
+                sampleItem.setReference("poName", poTermItem.getIdentifier());
+                if (!organGroup.isEmpty()) sampleItem.setAttribute("organGroup", organGroup);
+                if (!tissueDescription.isEmpty()) sampleItem.setAttribute("tissueDescription", tissueDescription);
+                sampleMap.put(sampleName, sampleItem);
+            }
 
+            // create Replicate item
+            Item item = createItem("Replicate");
+            item.setAttribute("name", sampleReplicateName);
+            item.setAttribute("sampleReplicateName", sampleReplicateName);
 
+            if (!sraBioSampleId.isEmpty()) item.setAttribute("sraBioSampleId", sraBioSampleId);
+            if (!sraRunId.isEmpty()) item.setAttribute("sraRunId", sraRunId);
+            if (!sraExperimentId.isEmpty()) item.setAttribute("sraExperimentId", sraExperimentId);
+            if (!sraSampleId.isEmpty()) item.setAttribute("sraSampleId", sraSampleId);
+            if (!bioSampleDescription.isEmpty()) item.setAttribute("bioSampleDescription", bioSampleDescription);
+            if (!replicateNumber.isEmpty()) item.setAttribute("replicateNumber", replicateNumber);
 
+            // set Sample as sample reference for Replicate
+            item.setReference("sample", sampleItem.getIdentifier());
+            // add to Sample's replicates collection
+            sampleItem.addToCollection("replicates", item);
 
-  
-
-            if (!sampleMean.isEmpty()) { item.setAttribute("sampleMean", sampleMean); }
-            if (!bioSample.isEmpty()) { item.setAttribute("bioSample", bioSample); }
-            if (!run.isEmpty()) { item.setAttribute("run", run); }
-           if (!experiment.isEmpty()) { item.setAttribute("experiment", experiment); }
-            if (!SRAStudy.isEmpty()) { item.setAttribute("SRAStudy", SRAStudy); }
-            if (!bioProject.isEmpty()) { item.setAttribute("bioProject", bioProject); }
-            if (!libraryLayout.isEmpty()) { item.setAttribute("libraryLayout", libraryLayout); }
-            if (!bioSample_description.isEmpty()) { item.setAttribute("bioSample_description", bioSample_description); }
-             if (!sample.isEmpty()) { item.setAttribute("sample", sample); }
-            if (!organ_group.isEmpty()) { item.setAttribute("organ_group", organ_group); }
-            if (!tissue_description.isEmpty()) { item.setAttribute("tissue_description", tissue_description); }
-            if (!PO_term.isEmpty()) { item.setAttribute("PO_term", PO_term); }
-            if (!PO_name.isEmpty()) { item.setAttribute("PO_name", PO_name); }
-            if (!growth_stage.isEmpty()) { item.setAttribute("growth_stage", growth_stage); }
-            if (!replicate.isEmpty()) { item.setAttribute("replicate", replicate); }
-          
-
-
-
-
-
-
-
-            
-            
             try {
+                // store Replicate item
                 store(item);
-            } catch(Exception e) {
-                System.out.println("Error while storing ExpressionMetadata item: " + item + "\nStacktrace: " + e);
+            } catch (Exception e) {
+                System.out.println("Error while storing item: " + item + "\n" + e);
             }
         }
     }
 
+    /**
+     * Get an Item representation of Plant Ontology term
+     * @param termName
+     * @param ontologyName
+     * @return
+     */
+    private Item getPlantOntologyTerm(String identifier, String name) {
+        Item ontologyTerm = null;
+        if (ontologyTermMap.containsKey(identifier)) {
+            ontologyTerm = ontologyTermMap.get(identifier);
+        }
+        else {
+            ontologyTerm = createItem("POTerm");
+            ontologyTerm.setAttribute("identifier", identifier);
+            ontologyTerm.setAttribute("name", name);
+            Item ontology = getOntology(PLANT_ONTOLOGY);
+            ontologyTerm.setReference("ontology", ontology);
+            ontologyTermMap.put(identifier, ontologyTerm);
+        }
+        return ontologyTerm;
+    }
 
+    /**
+     * Get an Item representation of an Ontology
+     * @param ontologyName
+     * @return
+     */
+    private Item getOntology(String ontologyName) {
+        Item ontology = null;
+        if (delayedItems.containsKey(ontologyName)) {
+            ontology = delayedItems.get(ontologyName);
+        }
+        else {
+            ontology = createItem("Ontology");
+            ontology.setAttribute("name", ontologyName);
+            ontology.setAttribute("url", "https://bioportal.bioontology.org/ontologies/PO");
+            delayedItems.put(ontologyName, ontology);
+        }
+        return ontology;
+    }
+
+    /**
+     *
+     */
+    public void storeItems() {
+        for (String key : sampleMap.keySet()) {
+            try {
+                // store Sample item
+                store(sampleMap.get(key));
+            } catch (Exception e) {
+                System.out.println("Error while storing sample item:\n" + sampleMap.get(key) + "\nStackTrace:\n" + e);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public void storeOntologyTermItems() {
+        for (String key : ontologyTermMap.keySet()) {
+            try {
+                // store Ontology term item
+                store(ontologyTermMap.get(key));
+            } catch (Exception e) {
+                System.out.println("Error while storing ontology term item:\n" + ontologyTermMap.get(key) + "\nStackTrace:\n" + e);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public void storeDelayedItems() {
+        for (String key : delayedItems.keySet()) {
+            try {
+                // store Sample item
+                store(delayedItems.get(key));
+            } catch (Exception e) {
+                System.out.println("Error while storing delayed item:\n" + delayedItems.get(key) + "\nStackTrace:\n" + e);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public void close() {
+        storeItems();
+        storeOntologyTermItems();
+        storeDelayedItems();
+    }
 }
